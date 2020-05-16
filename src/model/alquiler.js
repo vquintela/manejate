50cd5ed3f055e550684fe395819ec4b8a9e1a9e0
hernanrago <hernanrago@msn.com>
2020-05-16 18:39:20 -0300

    Agregado modelo de alquiler.

---
+++ src/model/alquiler.js
@@ -0,0 +1,35 @@
+import { Schaema, model } from 'mongoose';
+const uniqueValidator = require('mongoose-unique-validator');
+
+const alquilerSchema = new Schema({
+    fechaEntrega:{
+        type: Date
+    },
+    fechaDevolucion:{
+        type: Date
+    },
+    fechaReserva:{
+        type: Date
+    },
+    fechaCancelacion: {
+        type: Date
+    },
+    usuario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    motocicleta: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'moto'
+    },
+    sedeEntrega: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sede'
+    },
+    sedeDevolucion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'sede'
+    },
+});
+
+module.exports = model('alquiler', alquilerSchema);
