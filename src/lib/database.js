05c89bcbb7bed1af36972761561d42e6dc57296d
hernanrago <hernanrago@msn.com>
2020-06-30 23:17:59 -0300

    Connection string env variable 3.

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 //Direccion db
-const direccion = "mongodb://127.0.0.1:27017/manejate";
+const direccion = "mongodb+srv://hrago:rv-300KH@manejate.kbg2x.mongodb.net/manejate";
+// const direccion = "mongodb://127.0.0.1:27017/manejate";
+
 
 mongoose.connect(direccion, {
     useNewUrlParser: true,
