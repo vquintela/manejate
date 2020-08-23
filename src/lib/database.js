4b6a690ee1f9488228ef69e297d0b8506ecb52f3
hernanrago <hernanrago@msn.com>
2020-08-23 11:39:06 -0300

    Base remota.

--- src/lib/database.js
+++ src/lib/database.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 //Direccion db
 // const direccion = "mongodb+srv://hrago:rv-300KH@manejate.kbg2x.mongodb.net/manejate";
-const direccion = "mongodb://127.0.0.1:27017/manejate";
+//const direccion = "mongodb://127.0.0.1:27017/manejate";
 // Victor
-// const direccion = "mongodb+srv://manejate:Manejate2020@cluster0.j64nl.mongodb.net/manejate?retryWrites=true&w=majority"
+const direccion = "mongodb+srv://manejate:Manejate2020@cluster0.j64nl.mongodb.net/manejate?retryWrites=true&w=majority"
 
 mongoose.connect(direccion, {
     useNewUrlParser: true,
