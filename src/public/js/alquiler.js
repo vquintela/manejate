86893701e592d1e9e2ef6a9dbfc4009d91c6647b
vitor <vitor@vitor>
2020-09-13 00:10:58 -0300

    footer y vista moto alquiler

--- src/public/js/alquiler.js
+++ src/public/js/alquiler.js
@@ -6,6 +6,12 @@ const cancelarAlquiler = () => {
     console.log(this.attributes['cancelar-alquiler'].value);
 };
 
+const verMoto = async (id) => {
+  const motoJSON = await fetch("/motos/editar/" + id, { method: "GET" });
+  const res = await JSON.parse(await motoJSON.text());
+  console.log(res)
+}
+
 document
   .querySelectorAll("[editar-alquiler]")
   .forEach((e) => e.addEventListener("click", editarAlquiler));
@@ -13,3 +19,8 @@ document
 document
   .querySelectorAll("[cancelar-alquiler]")
   .forEach((e) => e.addEventListener("click", cancelarAlquiler));
+
+
+document
+  .querySelectorAll('.ver-moto')
+  .forEach((e) => e.addEventListener("click", () => verMoto(e.getAttribute('moto-id'))));
